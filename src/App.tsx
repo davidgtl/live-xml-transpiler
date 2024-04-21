3702a7fe20dc274f09a8e6c3d9618b142617fe39
davidgtl <david.ghitulescu@gmail.com>
2024-04-21 22:13:16 +0300

    bidiectional yaml xml

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useEffect, useState } from 'react'
+import CodeMirror from '@uiw/react-codemirror';
+import { xml as langXML } from '@codemirror/lang-xml';
+import { yaml as langYAML } from '@codemirror/lang-yaml';
+import { atomone } from '@uiw/codemirror-theme-atomone';
+import XML from 'xml2js';
+import YAML from 'yaml'
+
 import './App.css'
+import { demoXML } from './demoXML';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [valueXML, setValueXML] = useState(demoXML);
+  const onChangeXML = useCallback((val: any, _viewUpdate) => {
+    console.log('val:', val);
+    setValueXML(val);
+    XML.parseString(val, (err, result) => {
+      console.dir(result);
+      setValueReq(YAML.stringify(result));
+    });
+  }, []);
+
+  const [valueReq, setValueReq] = useState("");
+  const onChangeReq = useCallback((val: any, _viewUpdate) => {
+    console.log('val:', val);
+    setValueReq(val);
+    
+    const builder = new XML.Builder();
+    const xml = builder.buildObject(YAML.parse(val));
+    setValueXML(xml);
+  }, []);
+
+  useEffect(() => {
+    XML.parseString(demoXML, (err, result) => {
+      console.dir(result);
+      setValueReq(YAML.stringify(result));
+    });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="h-full w-full flex flex-row">
+      <div className='flex-1' />
+      <CodeMirror
+        value={valueReq}
+        theme={atomone}
+        width="48vw"
+        height="95vh"
+        extensions={[langYAML()]}
+        onChange={onChangeReq} />
+      <CodeMirror value={valueXML}
+        theme={atomone}
+        width="48vw"
+        height="95vh"
+        extensions={[langXML({})]}
+        onChange={onChangeXML} />
+      <div className='flex-1' />
+    </div>
   )
 }
 
